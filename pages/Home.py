ab24d18b5c54e2712e4638d48192b83acc2117bb
RotX18 <ryanohtzexuan@gmail.com>
2025-10-19 21:14:12 +0800

    Entry pt is now in root folder. Added utils file with product class def and generate receipt func. Added basic dates for discounts

--- pages/Home.py
+++ pages/Home.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
-pages = [st.Page("Home.py", title="Home",),
-	 st.Page("AboutUs.py", title="About Us"), 
-	 st.Page("Menu.py", title="Menu"),
-	 st.Page("Ingredients.py", title="Ingredients"), 
-	 st.Page("Contact.py", title="Contact Us")]
+st.header("WELCOME to the AOM GOM empire!")
 
-st.navigation(pages, position="top").run()
-
+st.image("./res/img/hi.jpg")

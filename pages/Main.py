02049b0cd13e7c7fa5002a64cc8d2c748a61b5c4
zwovmov <aomaom1294529@gmail.com>
2025-10-20 15:13:41 +0800

    Update Home.py and add Main.py page

---
+++ pages/Main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from upload_menu import db
+
+pages = {
+	"":[st.Page("Home.py", title="Home",),st.Page("AboutUs.py", title="About Us"), st.Page("Menu.py", title="Menu"), st.Page("Ingredients.py", title="Ingredients"), st.Page("Contact.py", title="Contact Us")]
+}
+st.navigation(pages, position="top").run()
+
